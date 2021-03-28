f03791742d06e642418ee77833150e308da2dc91
TULIMAT Anas OBS/CSO <TZKM2460@orange.com>
2021-03-28 14:34:36 +0200

    feat: add react-redux and create redux store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import PhoneComponent from "./component/PhoneComponent";
+import store from "./redux/store";
+import {Provider} from "react-redux";
 
 function App() {
   return (
-    <div className="App">
-      <PhoneComponent />
-    </div>
+    <Provider store={store}>
+        <div className="App">
+            <PhoneComponent />
+        </div>
+    </Provider>
   );
 }
 

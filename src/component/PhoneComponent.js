d8c727c5e93fa7e07d7f98a1b564e0e1710dce0b
TULIMAT Anas OBS/CSO <TZKM2460@orange.com>
2021-03-28 14:21:35 +0200

    init: add phone componenet with redux state

---
+++ src/component/PhoneComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const PhoneComponent = () => {
+
+    return (
+        <div className="container">
+            <h1>Phone</h1>
+            <p><span></span></p>
+            <button>Buy</button>
+        </div>
+    )
+}
+
+export default PhoneComponent;
---
+++ src/redux/phone/actionPhone.js
@@ -0,0 +1,11 @@
+import { BUY_PHONE } from "./type";
+
+/**
+ * Creation of action creator
+ * @returns {{type: string}}
+ */
+export const buyPhone = () => {
+    return {
+        type: BUY_PHONE
+    }
+}

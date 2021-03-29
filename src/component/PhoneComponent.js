7fffd092b4ef48d69429f0823e0508a9a8f0d357
TULIMAT Anas OBS/CSO <TZKM2460@orange.com>
2021-03-29 11:24:34 +0200

    feat: add redux mapDispatchToProps to phone component

--- src/component/PhoneComponent.js
+++ src/component/PhoneComponent.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import {Card, Button} from "react-bootstrap";
 import {connect} from "react-redux";
+import {buyPhone} from "../redux/phone/actionPhone";
 
 const PhoneComponent = (props) => {
+
+    console.log(props);
 
     return (
         <Card>
@@ -11,16 +14,37 @@ const PhoneComponent = (props) => {
                 <Card.Text>
                     Disponible : {props.phones}
                 </Card.Text>
-                <Button variant="primary">Buy</Button>
+                <Button
+                    variant="primary"
+                    onClick={() => props.buyPhone()}
+                >
+                    Buy
+                </Button>
             </Card.Body>
         </Card>
     )
 }
 
+/**
+ * Redux method, help to get state from redux store and pass it to component via props
+ * @param state
+ * @returns {{phones: (number|*)}}
+ */
 const mapStateToProps = (state) => {
     return {
         phones: state.phones
     }
 }
 
-export default connect(mapStateToProps)(PhoneComponent);
+/**
+ * Redux method, help to pass action from redux store to our component's props
+ * @param dispatch
+ * @returns {{buyPhone: (function(): *)}}
+ */
+const mapDispatchToProps = (dispatch) => {
+    return {
+        buyPhone: () => dispatch(buyPhone())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PhoneComponent);
